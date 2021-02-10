92fae953bc688d4655f431d71876dc3004d6ee27
Soloman Garcia <soloman.garcia@gmail.com>
2021-02-10 11:28:20 -0700

    create setupDatePicker function

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-import { format } from "date-fns";
+import { format, getUnixTime, fromUnixTime } from "date-fns";
 
 const datePickerButton = document.querySelector(".date-picker-button");
 const datePicker = document.querySelector(".date-picker");
+const datePickerHeaderText = document.querySelector(".current-month");
 
 datePickerButton.addEventListener("click", () => {
   datePicker.classList.toggle("show");
+  const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate);
+  setupDatePicker(selectedDate);
 });
 
 function setDate(date) {
   datePickerButton.innerText = format(date, "MMMM do, yyyy");
+  datePickerButton.dataset.selectedDate = getUnixTime(date);
+}
+
+function setupDatePicker(selectedDate) {
+  datePickerHeaderText.innerText = format(selectedDate, "MMMM - yyyy");
 }
 
 setDate(new Date());
