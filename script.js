0e6035a55d4eeed98b562e397dc371d62601e11d
Soloman Garcia <soloman.garcia@gmail.com>
2021-02-10 12:04:55 -0700

    finish setupDates function

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ import {
   startOfMonth,
   endOfWeek,
   endOfMonth,
-  eachDayOfInterval
+  eachDayOfInterval,
+  isSameMonth,
+  isSameDay
 } from "date-fns";
 
 const datePickerButton = document.querySelector(".date-picker-button");
@@ -46,18 +48,32 @@ function setupDates(selectedDate) {
     const dateElement = document.createElement("button");
     dateElement.classList.add("date");
     dateElement.innerText = date.getDate();
+    if (!isSameMonth(date, currentDate)) {
+      dateElement.classList.add("date-picker-other-month-date");
+    }
+    if (isSameDay(date, selectedDate)) {
+      dateElement.classList.add("selected");
+    }
+
+    dateElement.addEventListener("click", () => {
+      setDate(date);
+      datePicker.classList.remove("show");
+    });
+
     dateGrid.appendChild(dateElement);
   });
 }
 
 nextMonthButton.addEventListener("click", () => {
+  const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate);
   currentDate = addMonths(currentDate, 1);
-  setupDatePicker();
+  setupDatePicker(selectedDate);
 });
 
 previousMonthButton.addEventListener("click", () => {
+  const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate);
   currentDate = subMonths(currentDate, 1);
-  setupDatePicker();
+  setupDatePicker(selectedDate);
 });
 
 setDate(new Date());
